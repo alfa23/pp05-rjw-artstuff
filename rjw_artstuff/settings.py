82a7d815ebce395c661819ab569452c6a9999e1d
alfa23 <paw235@gmail.com>
2023-01-29 11:16:08 +0000

    deploy: Update secret key & set debug

--- rjw_artstuff/settings.py
+++ rjw_artstuff/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zgfpc#8blk2$2j!c2quifmt0af3ske##y87n9%ub*_-)+6=$qt'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pp05-rjw-artstuff.herokuapp.com', 'localhost']
 
