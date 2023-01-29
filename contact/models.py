5108e3c09d54c6cb18f1e1eab9a294898f070ff7
alfa23 <paw235@gmail.com>
2023-01-29 20:29:29 +0000

    add: Contact & newsletter models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """ A model for the customer contact form """
+
+    first_name = models.CharField(max_length=254, null=False, blank=False)
+    last_name = models.CharField(max_length=254, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    subject = models.CharField(max_length=100, null=False, blank=False)
+    message = models.TextField(max_length=600, null=False, blank=False)
+    mobile = models.CharField(max_length=25, null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+class Newletter(models.Model):
+    """ A model for the customer newsletter subscription form """
+
+    email = models.EmailField(max_length=254, null=False, blank=False)
+
+    def __str__(self):
+        return self.email
