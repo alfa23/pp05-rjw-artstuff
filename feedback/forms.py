87bac34428272053c7dc0b7ac9f4e5837d2a9e91
alfa23 <paw235@gmail.com>
2023-01-31 19:33:32 +0000

    add: Further code & styling to view & add feedback

--- feedback/forms.py
+++ feedback/forms.py
@@ -13,3 +13,26 @@ class FeedbackForm(forms.ModelForm):
                 'min': '0',  # Sets minimum value
             })
         }
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'title': 'Title',
+            'body': 'Your feedback',
+            'user_rating': 'Your rating (0-5)',
+        }
+
+        self.fields['title'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-accent \
+                rounded-0 feedback-form-input'
+            self.fields[field].label = False
